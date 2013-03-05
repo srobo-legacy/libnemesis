b7bcab6a6286d7823bbd3c687babf95e7ceb5d58
Sam Phippen <samphippen@googlemail.com>
2013-03-05 10:46:38 -0800

    Many tests
    Signed-off-by: Sam Phippen <samphippen@googlemail.com>

--- tests/user_tests.py
+++ tests/user_tests.py
@@ -29,7 +29,7 @@ def test_is_teacher_2():
     assert User.create_user("teacher_coll1").is_teacher
 
 def test_is_blueshirt_1():
-    assert User.create_user("blueshirt_1").is_blueshirt
+    assert User.create_user("blueshirt").is_blueshirt
 
 def test_user_equality():
     a = User.create_user("teacher_coll1")
@@ -50,3 +50,45 @@ def test_authed_user_nequality():
     a = User.create_user("teacher_coll1", "facebees")
     b = User.create_user("teacher_coll2")
     assert a != b
+
+def test_unauthed_cant_see_any():
+    a = User.create_user("teacher_coll1")
+    users = ["teacher_coll1", "student_coll1_1", "student_coll1_2"]
+    results = [a.can_administrate(user) for user in users]
+    assert not any(results)
+
+def test_authed_can_see_self():
+    user_passwords = [("teacher_coll1", "facebees"),
+                      ("student_coll1_1", "cows"),
+                      ("teacher_coll2", "noway"),
+                      ("blueshirt", "blueshirt")
+                     ]
+
+    user_objects = [User.create_user(u[0],u[1]) for u in user_passwords]
+    results = [user.can_administrate(user.username) for user in user_objects]
+
+    assert all(results)
+
+def test_authed_teacher_can_see_own_students():
+    a = User.create_user("teacher_coll1", "facebees")
+    users = ["student_coll1_1", "student_coll1_2"]
+
+    results = [a.can_administrate(user) for user in users]
+
+    assert all(results)
+
+def test_authed_teacher_cant_see_other_students():
+    a = User.create_user("teacher_coll1", "facebees")
+    users = ["student_coll2_1", "student_coll2_2"]
+
+    results = [a.can_administrate(user) for user in users]
+
+    assert not any(results)
+
+def test_authed_teachers_cant_see_blueshirt():
+    u = User.create_user("teacher_coll1", "facebees")
+    u2 = User.create_user("teacher_coll2", "noway")
+    users = [u, u2]
+    a = User.create_user("blueshirt")
+
+    assert not any([u.can_administrate(a) for u in users])
