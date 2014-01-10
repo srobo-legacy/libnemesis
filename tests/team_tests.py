ada87b5e080178a07fac708c530b4d2fa8a17bf4
Peter Law <PeterJCLaw@gmail.com>
2014-01-10 22:36:05 +0000

    Add a bunch of team tests.
    These probably should have been added ages ago..
    
    Change-Id: I5ff1d953afad83ac50dc23f5ce5e5c641c1dbf88

--- tests/team_tests.py
+++ tests/team_tests.py
@@ -6,3 +6,24 @@ def test_team_has_right_users():
     expected = set(["teacher_coll1", "student_coll1_1", "withdrawn_student"])
     assert users == expected
 
+def test_team_not_eq_none():
+    t = Team('team-ABC')
+    assert t != None
+
+def test_team_name():
+    t = Team('team-ABC')
+    assert t.name == 'team-ABC'
+
+def test_team_not_eq_string():
+    t = Team('team-ABC')
+    assert t != 'team-ABC'
+
+def test_team_not_eq_other_team():
+    t1 = Team('team-ABC')
+    t2 = Team('team-DEF')
+    assert t1 != t2
+
+def test_team_eq_equivalent_instance():
+    t1 = Team('team-ABC')
+    t2 = Team('team-ABC')
+    assert t1 == t2
