f1b8d1a512f96e035a91ed721ae775f2fa924fb1
Peter Law <PeterJCLaw@gmail.com>
2014-01-10 22:29:26 +0000

    Tidy code to thin it.
    Change-Id: I0b7c2a406ddaf5b5b300292ce5d3a7f504d0149f

--- tests/team_tests.py
+++ tests/team_tests.py
@@ -2,5 +2,7 @@ from libnemesis import Team
 
 def test_team_has_right_users():
     t = Team("team-ABC")
-    assert set([u.username for u in t.users]) == set(["teacher_coll1", "student_coll1_1"])
+    users = set(u.username for u in t.users)
+    expected = set(["teacher_coll1", "student_coll1_1"])
+    assert users == expected
 
